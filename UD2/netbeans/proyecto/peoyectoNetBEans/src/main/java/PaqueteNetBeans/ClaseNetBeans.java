30c23227e49cbddbcc458392edd0565b6d513a2d
Hernan Coronel <dam.hernan.coronel@gmail.com>
2025-02-05 18:15:07 +0100

    cosas netbeans

---
+++ UD2/netbeans/proyecto/peoyectoNetBEans/src/main/java/PaqueteNetBeans/ClaseNetBeans.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PaqueteNetBeans;
+
+/**
+ *
+ * @author paz
+ */
+public class ClaseNetBeans {
+    public static void main(String[] args){
+        System.out.println("Hola mundo!");
+    }    
+}
